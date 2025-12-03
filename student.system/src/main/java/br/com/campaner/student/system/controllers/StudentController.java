6b9335101bb31b7eb70372634ce96eb3651de97a
unknown <luis.henri.campaner@gmail.com>
2025-12-02 22:54:11 -0300

    Adicionando a parte backend ao projeto

---
+++ student.system/src/main/java/br/com/campaner/student/system/controllers/StudentController.java
@@ -0,0 +1,12 @@
+package br.com.campaner.student.system.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/student")
+@RestController
+public class StudentController {
+
+    
+
+}
---
+++ student.system/src/test/java/br/com/campaner/student/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.campaner.student.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
